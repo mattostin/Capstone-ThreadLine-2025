3e137ad27155ff7b614f69440b6eea427e8bd024
Matthew Ostin <matthewostin@gmail.com>
2025-07-05 16:57:34 -0700

    hg

--- php/signup.php
+++ php/signup.php
@@ -24,7 +24,7 @@ echo <<<HTML
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Lilita+One&family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
-  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="../style.css" />
 </head>
 <body>
 <nav class="navbar">
