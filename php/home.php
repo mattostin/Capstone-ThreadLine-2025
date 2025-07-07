5ba8b032a7b5729c6221a7076e0c2d60a3f870ac
Matthew Ostin <matthewostin@gmail.com>
2025-07-06 19:22:18 -0700

    h

--- php/home.php
+++ php/home.php
@@ -19,7 +19,6 @@ $username = ucfirst(htmlspecialchars($_SESSION['username']));
   <style>
     body {
       margin: 0;
-      font-family: 'Lilita One', cursive;
       background: linear-gradient(to bottom, #1071977a 0%, #88b9e9 50%, #075eb6 100%);
       background-repeat: no-repeat;
       background-attachment: fixed;
