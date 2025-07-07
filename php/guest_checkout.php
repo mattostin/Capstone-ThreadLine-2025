3dbc3f752cb72385bcfa59af8dc4aab07bb40f99
Matthew Ostin <matthewostin@gmail.com>
2025-07-07 16:22:02 -0700

    h

--- php/guest_checkout.php
+++ php/guest_checkout.php
@@ -5,11 +5,7 @@ session_set_cookie_params([
   'samesite' => 'Strict'
 ]);
 session_start();
-
-// Mark the user as a guest
 $_SESSION['guest'] = true;
-
-// Redirect to the actual checkout page
 header("Location: checkout.php");
 exit;
 ?>
