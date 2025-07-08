b4884d38d31b9e5a49e4c063f37bd6f35ff1a4d0
Matthew Ostin <matthewostin@gmail.com>
2025-07-07 17:44:58 -0700

    h

--- php/checkout.php
+++ php/checkout.php
@@ -14,7 +14,7 @@ if (session_status() === PHP_SESSION_NONE) {
   <meta charset="UTF-8" />
   <title>Checkout - ThreadLine</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="...css/style.css" />
+  <link rel="stylesheet" href="css/style.css" />
   <style>
     body {
       font-family: 'Poppins', sans-serif;
