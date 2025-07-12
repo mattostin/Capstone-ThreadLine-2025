7622f5e5e586845ea723302c294cfaea5a1bf70f
Matthew Ostin <matthewostin@gmail.com>
2025-07-12 15:38:03 -0700

    h

--- php/signup.php
+++ php/signup.php
@@ -57,6 +57,27 @@ echo <<<HTML
   </ul>
 </nav>
 <div class="signup-container">
+  <form method="POST" action="">
+    <label for="first_name">First Name:</label>
+    <input type="text" name="first_name" required /><br><br>
+
+    <label for="last_name">Last Name:</label>
+    <input type="text" name="last_name" required /><br><br>
+
+    <label for="username">Username:</label>
+    <input type="text" name="username" required /><br><br>
+
+    <label for="email">Email:</label>
+    <input type="email" name="email" required /><br><br>
+
+    <label for="dob">Date of Birth:</label>
+    <input type="date" name="dob" required /><br><br>
+
+    <label for="password">Password:</label>
+    <input type="password" name="password" required /><br><br>
+
+    <button type="submit">Sign Up</button>
+  </form>
 HTML;
 
 // Check connection
