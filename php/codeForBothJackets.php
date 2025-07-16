db3edaaf1fb212a597d57bc88a6fd8b9d8f3b8e8
CJennings23 <164796053+CJennings23@users.noreply.github.com>
2025-07-16 00:31:56 -0400

    Update codeForBothJackets.php

--- php/codeForBothJackets.php
+++ php/codeForBothJackets.php
@@ -112,7 +112,7 @@ if (isset($_SESSION['user_id'])) {
   </nav>
 
   <main>
-<h1 class="section-title">Our Products</h1>
+<h1 class="section-title">Our</h1>
     <div class="container">
       <div class="product-grid">
         <?php
