2f4bd7dd7fd6ad51ca2672a8bd3c9e002993eb2d
Matthew Ostin <matthewostin@gmail.com>
2025-07-16 20:55:27 -0700

    h

--- php/codeForBothJackets.php
+++ php/codeForBothJackets.php
@@ -112,7 +112,7 @@ if (isset($_SESSION['user_id'])) {
   </nav>
 
   <main>
-  <h1 class="section-title">Our__Products</h1>
+    <h1 style="text-align:center; color: white;"> Our Products</h1>
     <div class="container">
       <div class="product-grid">
         <?php
